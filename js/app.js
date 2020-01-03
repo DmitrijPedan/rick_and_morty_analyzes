0779fbf956478e2bdd6c46e47cc56a014da3e904
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-01-03 17:16:42 +0200

    add clb

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 /* Rick and Morty analyzes */
-// console.log("Rick and Morty analyzes is started ...");
-
 
 const url = 'https://rickandmortyapi.com/api/character';
 
+const getData = function(clb) {
 axios
     .get(url)
     .then(response => {
@@ -11,13 +10,39 @@ axios
         const links = [...new Array(count)].map((el,i) => `${url}/?page=${i+1}`);
         Promise.all(links.map(el => axios.get(el)))
             .then(response => {
-                const characters = ([...new Array(response.length)].map((el,i) => el = response[i].data.results)).flat();
-                console.log(characters);
-                
+                const characters = ([...new Array(response.length)]
+                    .map((el,i) => el = response[i].data.results))
+                    .flat();
+                clb(characters);
+                // console.log(clb);
+                // console.log(characters);
             })
             .catch(error => console.log('Error in all Promise', error));
     })
     .catch(error => console.log('Error in main Promise', error));
+};
+
+const getCharactersInfo = (characters) => {
+    const names = characters.map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, пол: ${el.gender}, статус: ${el.status}`);
+    console.log(names);
+};
+
+const getGenderCharacters = (characters) => {
+    const genders = {1: "Male", 2: "Female", 3: "Genderless", 4: "unknown"};
+    const result = characters
+        .filter(el => el.gender === (genders[2]))
+        .map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, статус: ${el.status}`);
+    console.log(result);
+};
+
+const getHumansCharacters = (characters) => {
+    const genders = {1: "Male", 2: "Female", 3: "Genderless", 4: "unknown"};
+    const result = characters
+        .filter(el => el.gender === (genders[2]))
+        .map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, пол: ${el.gender}, статус: ${el.status}`);
+    console.log(result);
+};
 
 
 
+getData(getGenderCharacters);
