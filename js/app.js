194d859a58229baa997f29bb906234223ee1dc84
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-01-03 19:19:45 +0200

    add to local storage

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-/* Rick and Morty analyzes */
+/*==================== Rick and Morty analyzes ==========================*/
 
 const url = 'https://rickandmortyapi.com/api/character';
 
-const getData = function(clb) {
+let data = JSON.parse(localStorage.getItem("charKey"));
+
+const getData = function() {
 axios
     .get(url)
     .then(response => {
@@ -13,36 +15,62 @@ axios
                 const characters = ([...new Array(response.length)]
                     .map((el,i) => el = response[i].data.results))
                     .flat();
-                clb(characters);
-                // console.log(clb);
-                // console.log(characters);
+                localStorage.setItem("charKey", JSON.stringify(characters));
             })
             .catch(error => console.log('Error in all Promise', error));
     })
     .catch(error => console.log('Error in main Promise', error));
 };
 
-const getCharactersInfo = (characters) => {
-    const names = characters.map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, пол: ${el.gender}, статус: ${el.status}`);
+//============================================================================================
+
+// function newFunc () {
+//     console.log(document.getElementById("gender-input").value)
+// }
+// document.getElementById("get-input").onclick = newFunc;
+
+
+//==========  Local Storage ==========
+
+const deleteFromLs = () => {
+    localStorage.removeItem("charKey");
+};
+
+const getDataFromLS = function () {
+    if (localStorage.getItem("charKey")) {
+        data = JSON.parse(localStorage.getItem("charKey"));
+      }
+    console.log(data);
+}
+
+document.getElementById("write-ls").onclick = getData;
+document.getElementById("write-data").onclick = getDataFromLS;
+document.getElementById("delete-ls").onclick = deleteFromLs;
+ 
+//==========  Analyzes ==========
+
+ const getCharactersInfo = () => {
+    const names = data.map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, пол: ${el.gender}, статус: ${el.status}`);
     console.log(names);
 };
-
-const getGenderCharacters = (characters) => {
-    const genders = {1: "Male", 2: "Female", 3: "Genderless", 4: "unknown"};
-    const result = characters
-        .filter(el => el.gender === (genders[2]))
-        .map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, статус: ${el.status}`);
-    console.log(result);
-};
-
-const getHumansCharacters = (characters) => {
-    const genders = {1: "Male", 2: "Female", 3: "Genderless", 4: "unknown"};
-    const result = characters
-        .filter(el => el.gender === (genders[2]))
-        .map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, пол: ${el.gender}, статус: ${el.status}`);
-    console.log(result);
-};
+console.log(data);
+getCharactersInfo();
 
 
+// const getGenderCharacters = (characters) => {
+//     const genders = {1: "Male", 2: "Female", 3: "Genderless", 4: "unknown"};
+//     const result = characters
+//         .filter(el => el.gender === (genders[2]))
+//         .map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, статус: ${el.status}`);
+//     console.log(result);
+// };
 
-getData(getGenderCharacters);
+// const getHumansCharacters = (characters) => {
+//     const genders = {1: "Male", 2: "Female", 3: "Genderless", 4: "unknown"};
+//     const result = characters
+//         .filter(el => el.gender === (genders[2]))
+//         .map(el => `ID: ${el.id}, Имя: ${el.name}, вид: ${el.species}, пол: ${el.gender}, статус: ${el.status}`);
+//     console.log(result);
+// };
+
+
